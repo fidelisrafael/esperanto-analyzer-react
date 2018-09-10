879824897e64f977f602903a5e52cdffdaa1fee4
Rafael Fidelis <rafa_fidelis@yahoo.com.br>
2018-09-10 14:41:56 -0300

    Added tests for <GithubRibbon />

--- src/Components/GithubRibbon/GithubRibbon.jsx
+++ src/Components/GithubRibbon/GithubRibbon.jsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
-const DEFAULT_RIBBON_IMG = "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
 const DEFAULT_STYLE = {
   'position': 'absolute', 'top': 0, 'right': 0, 'border': 0
 }
+export const DEFAULT_RIBBON_IMG = "https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png"
 
 const GithubRibbon = (props) => {
   const {
-    href,
+    href = '',
     src = DEFAULT_RIBBON_IMG,
     alt = 'Fork me on Github',
     style = DEFAULT_STYLE,
-    target = '_blank'
+    target = '_blank',
+    isTest = null
   } = props
 
   return (
-    <a href={href} target={target} className='github-ribbon'>
+    <a isTesting={isTest} href={href} target={target}>
       <img
         style={style}
         src={src}
--- src/Components/GithubRibbon/GithubRibbon.test.jsx
+++ src/Components/GithubRibbon/GithubRibbon.test.jsx
@@ -1,10 +1,68 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import GithubRibbon from './GithubRibbon';
+import GithubRibbon, { DEFAULT_RIBBON_IMG, DEFAULT_STYLE } from './GithubRibbon';
 
-it('renders <GithubRibbon /> without crashing', () => {
-  const div = document.createElement('div');
+describe('<GithubRibbon />', () => {
+  it('renders without crashing', () => {
+    expect(<GithubRibbon />).toBeDefined()
+  })
 
-  ReactDOM.render(<GithubRibbon />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+  it('renders with default <img `src` /> when not provided', () => {
+    const ribbon = <GithubRibbon />
+ 
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const renderedRibbon = ribbon.type({}) // ribbon.type(props)
+
+    // Component props
+    expect(renderedRibbon.props.href).toEqual('')
+    // Rendered child <img>
+    expect(renderedRibbon.props.children.type).toEqual('img')
+    expect(renderedRibbon.props.children.props.src).toEqual(DEFAULT_RIBBON_IMG)
+  })
+
+  it('Should render with default `style`', () => {
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const component = (<GithubRibbon />).type({ href: 'http://github.com/fidelisrafael' })
+
+    expect(component.props.style).toEqual(DEFAULT_STYLE)
+  })
+
+  it('Should allows to override the default <img> `style` with a custom style', () => {
+    const style = {
+      'position': 'absolute', 'bottom': 0, 'right': 0, 'border': 1
+    }
+
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const component = (<GithubRibbon />).type({ style: style })
+
+    expect(component.props.children.props.style).toEqual(style)
+  })
+
+  it('Should provided on default `alt` attribute for <img>', () => {
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const component = (<GithubRibbon />).type({ href: 'http://github.com/fidelisrafael' })
+
+    expect(component.props.children.props.alt).toEqual('Fork me on Github')
+  })
+
+  it('Should allow to override the <a `alt` /> attribute', () => {
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const component = (<GithubRibbon />).type({ alt: 'Fork this project on Github' })
+
+    expect(component.props.children.props.alt).toEqual('Fork this project on Github')
+  })
+  
+  it('renders the <a> `src` attribute with the provided `href`', () => {
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const component = (<GithubRibbon />).type({ href: 'http://github.com/fidelisrafael' })
+
+    expect(component.props.href).toEqual('http://github.com/fidelisrafael')
+  })
+
+  it('Should opens the link in a "_blank" `target`', () => {
+    // Invokes the `Stateless function` and returns an already rendered `Symbol(react.element)`
+    const component = (<GithubRibbon />).type({ href: 'http://github.com/fidelisrafael' })
+
+    expect(component.props.target).toEqual('_blank')
+  })
+  
+})
